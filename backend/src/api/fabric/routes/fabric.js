fb961f61c8d1aed0723cd95399954cd2630deda0
yongPhuthan <86427357+yongPhuthan@users.noreply.github.com>
2022-05-06 16:34:01 +0700

    first commit

--- backend/src/api/fabric/routes/fabric.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * fabric router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::fabric.fabric');
--- backend/src/api/fabric/services/fabric.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * fabric service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::fabric.fabric');
